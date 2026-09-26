c3cc50d9352da44e3a711fffa6dd3083154afe34
Ankush Sihag <your-github-ankushsihag7@gmail.com>
2026-09-26 13:39:05 +0530

    starting server

--- smart_task/server.js
+++ smart_task/server.js
@@ -2,11 +2,15 @@ const express = require("express");
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.get("/api/health", (req, res) => {
     res.json({
         success: true,
         message: "Smart Task API is running"
     });
+});
+
+app.listen(PORT, () => {
+    console.log(`Smart Task API running on port ${PORT}`);
 });
